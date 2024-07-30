b9c5fd19b53e5bb8ad3369a812d37103097f7289
Michal Ciberej <michal.ciberej@gmail.com>
2024-07-31 00:24:29 +0200

    Add ToastContext, ToastList

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './styles/globals.css';
 import Header from '@/app/components/Header';
+import { ToastContextProvider } from './context/ToastContext';
+import { ToastList } from './components/Toast';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400'] });
 
@@ -19,8 +21,11 @@ export default function RootLayout({
     <html lang='en'>
       <body
         className={`${roboto.className} max-w-[1520px] w-full mx-auto bg-neutral-300 text-neutral-700 fill-neutral-700`}>
-        <Header />
-        {children}
+        <ToastContextProvider>
+          <Header />
+          {children}
+          <ToastList />
+        </ToastContextProvider>
       </body>
     </html>
   );
