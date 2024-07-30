660301ca4f8cf0d01a3eb23b61e4ebd48c082705
Michal Ciberej <michal.ciberej@gmail.com>
2024-07-31 00:26:10 +0200

    Add OrderWithProduct type declaration

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,5 +1,11 @@
+import { Prisma } from '@prisma/client';
+
 export type Route = {
   label: string;
   url: string;
   id: string;
 };
+
+export type OrderWithProduct = Prisma.OrderGetPayload<{
+  include: { product: true };
+}>;
