eb2ded70700151b60dd9e2e05c67a357d6c0b04e
Michal Ciberej <michal.ciberej@gmail.com>
2024-07-31 00:32:08 +0200

    Add layout, fetch content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <main className='py-4 px-2'>main page</main>;
-}
+import getProducts from '@/app/actions/get/getProducts';
+import InfiniteScroll from '@/app/components/InfiniteScroll';
+import Error from '@/app/components/Error';
+
+const NUMBER_OF_PRODUCTS = 12;
+
+const HomePage = async ({
+  searchParams,
+}: {
+  searchParams?: { page?: number };
+}) => {
+  const currentPage = Number(searchParams?.page || 1);
+
+  const { valid, products, message } = await getProducts(
+    NUMBER_OF_PRODUCTS * currentPage,
+    0
+  );
+
+  return (
+    <main>
+      <section className='py-4 px-2'>
+        {valid ? (
+          <InfiniteScroll
+            initialData={products}
+            records={NUMBER_OF_PRODUCTS}
+          />
+        ) : (
+          <Error message={message} />
+        )}
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
