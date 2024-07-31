408e57e5cabdbab52b317913cf3cc016f3fa2003
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-01 00:13:27 +0200

    Fix unused import

--- src/app/components/Toast/ToastList.tsx
+++ src/app/components/Toast/ToastList.tsx
@@ -4,7 +4,7 @@ import { useToastContext } from '@/app/context/ToastContext';
 import Toast from '@/app/components/Toast';
 
 const ToastList = () => {
-  const { toasts, removeToast } = useToastContext();
+  const { toasts } = useToastContext();
 
   return (
     <div className='fixed z-10 top-0 right-0 bottom-0 w-80 flex flex-col-reverse p-2 pointer-events-none gap-y-3'>
