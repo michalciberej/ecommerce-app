8b73465c5de36563d8d81af58f94e1039a07dc11
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-01 00:15:59 +0200

    Add first/last buttons, filter out non adjacent, add id

--- src/app/components/Pagination/Pagination.tsx
+++ src/app/components/Pagination/Pagination.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import { twMerge } from 'tailwind-merge';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { createPageURL } from '@/app/utils/createPageURL';
+import { twMerge } from 'tailwind-merge';
+import { v4 as uuid } from 'uuid';
+import {
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+} from '@heroicons/react/24/outline';
 
 const Pagination = ({ pagesCount }: { pagesCount: number }) => {
   const pathname = usePathname();
@@ -12,25 +17,59 @@ const Pagination = ({ pagesCount }: { pagesCount: number }) => {
 
   const buttons = Array(pagesCount)
     .fill(1)
-    .map((el, index) => el + index);
+    .map((el, index) => {
+      return { el: el + index, id: uuid() };
+    })
+    .filter(({ el }) => {
+      const prev = currentPage - 1;
+      const next = currentPage + 1;
+
+      const valid = el === prev || el === currentPage || el === next;
+
+      return valid;
+    });
+
+  if (pagesCount <= 0) return;
 
   return (
-    <div className='flex w-full justify-end'>
-      <div className='flex w-full max-w-fit items-end gap-x-2 pt-2'>
-        {buttons.map((page, index) => (
+    <nav className='flex w-full justify-end'>
+      <ul className='flex w-full max-w-fit items-end gap-x-2 pt-2'>
+        <li>
           <button
-            key={page + index}
             type='button'
-            onClick={() => replace(createPageURL(page, searchParams, pathname))}
-            className={twMerge(
-              'rounded-md px-3 py-1 ring-1 ring-inset ring-neutral-500 bg-neutral-50',
-              currentPage === page && 'ring-2 ring-orange-500'
-            )}>
-            {page}
+            aria-label={`Change to first page`}
+            onClick={() => replace(createPageURL(1, searchParams, pathname))}
+            className='rounded-md p-2 ring-1 ring-inset ring-neutral-500 bg-neutral-50 flex '>
+            <ChevronDoubleLeftIcon className='w-4 h-4' />
           </button>
+        </li>
+        {buttons.map(({ el, id }) => (
+          <li key={id}>
+            <button
+              type='button'
+              aria-label={`Change to ${el} page`}
+              onClick={() => replace(createPageURL(el, searchParams, pathname))}
+              className={twMerge(
+                'rounded-md px-3 py-1 ring-1 ring-inset ring-neutral-500 bg-neutral-50 flex',
+                currentPage === el && 'ring-2 ring-orange-500'
+              )}>
+              {el}
+            </button>
+          </li>
         ))}
-      </div>
-    </div>
+        <li>
+          <button
+            type='button'
+            aria-label={`Change to last page`}
+            onClick={() =>
+              replace(createPageURL(pagesCount, searchParams, pathname))
+            }
+            className='rounded-md p-2 ring-1 ring-inset ring-neutral-500 bg-neutral-50 flex'>
+            <ChevronDoubleRightIcon className='w-4 h-4' />
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
