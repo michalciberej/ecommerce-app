084522f91bff4fd992caca28e9236afaa00dd42c
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-01 00:03:35 +0200

    Remove searchParams, add Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,29 @@
 import getProducts from '@/app/actions/get/getProducts';
 import InfiniteScroll from '@/app/components/InfiniteScroll';
 import Error from '@/app/components/Error';
+import Footer from '@/app/components/Footer';
 
 const NUMBER_OF_PRODUCTS = 12;
 
-const HomePage = async ({
-  searchParams,
-}: {
-  searchParams?: { page?: number };
-}) => {
-  const currentPage = Number(searchParams?.page || 1);
-
-  const { valid, products, message } = await getProducts(
-    NUMBER_OF_PRODUCTS * currentPage,
-    0
-  );
+const HomePage = async () => {
+  const { valid, products, message } = await getProducts(NUMBER_OF_PRODUCTS, 0);
 
   return (
-    <main>
-      <section className='py-4 px-2'>
-        {valid ? (
-          <InfiniteScroll
-            initialData={products}
-            records={NUMBER_OF_PRODUCTS}
-          />
-        ) : (
-          <Error message={message} />
-        )}
-      </section>
-    </main>
+    <>
+      <main>
+        <section className='py-8 px-2'>
+          {valid ? (
+            <InfiniteScroll
+              initialData={products}
+              records={NUMBER_OF_PRODUCTS}
+            />
+          ) : (
+            <Error message={message} />
+          )}
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 };
 
