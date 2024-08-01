8614739ec48d702afbeed5a273692465390bfe7f
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-01 17:43:42 +0200

    Refactor ul to table

--- src/app/(pages)/list-of-orders/page.tsx
+++ src/app/(pages)/list-of-orders/page.tsx
@@ -24,45 +24,58 @@ const ListOfOrdersPage = async ({
       <section className='flex flex-col gap-4'>
         <h1 className='text-3xl'>List of Orders</h1>
         {valid ? (
-          <ul className='w-full bg-neutral-100 rounded-md shadow-md p-4 ring-1 ring-inset ring-neutral-500'>
-            <li className='w-full border-b border-neutral-500 py-2 justify-between flex items-center'>
-              <span className='sm:min-w-56 w-full'>Order Id</span>
-              <span className='min-w-20 w-full'>Price</span>
-              <span className='min-w-20 w-full'>Quantity</span>
-              <span className='min-w-20 w-full'>Total Price</span>
-              <span className='p-1'>
-                <XMarkIcon className='w-6 h-6 text-transparent' />
-              </span>
-            </li>
-            {orders.length > 0 ? (
-              orders.map((order) => (
-                <li
-                  key={order.id}
-                  className='flex py-2 [&:not(:last-child)]:border-b border-neutral-500 justify-between items-center'>
-                  <span className='sm:min-w-56 w-full sm:max-w-[20ch] truncate'>
-                    {order.id}
-                  </span>
-                  <span className='min-w-20 w-full'>{order.product.price}</span>
-                  <div className='min-w-20 w-full flex gap-1'>
-                    <span>{order.quantity}</span>
-                    <span>x</span>
-                  </div>
-                  <div className='min-w-20 w-full flex gap-1'>
-                    <span className='min-w-[4ch]'>
-                      {order.quantity * order.product.price}
-                    </span>
-                    <span>Kč</span>
-                  </div>
-                  <DeleteButton
-                    id={order.id}
-                    action={deleteOrder}
-                  />
-                </li>
-              ))
-            ) : (
-              <li className='w-full text-center py-6'>No orders yet</li>
-            )}
-          </ul>
+          <table className='w-full bg-neutral-100 rounded-md shadow-md p-4 ring-1 overflow-hidden ring-neutral-500'>
+            <thead>
+              <tr className='w-full border-b border-neutral-500 p-2 flex items-center justify-between sm:gap-8'>
+                <th className='text-start sm:max-w-56 sm:min-w-[20ch] w-full gap-1 sm:gap-8'>
+                  Order Id
+                </th>
+                <th className='text-start w-full'>Price</th>
+                <th className='text-start w-full'>Quantity</th>
+                <th className='text-start w-full'>Total Price</th>
+                <th className='p-1'>
+                  <XMarkIcon className='w-6 h-6 text-transparent' />
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.length > 0 ? (
+                orders.map((order, index) => (
+                  <tr
+                    key={order.id}
+                    className={`flex p-2 [&:not(:last-child)]:border-b border-neutral-500 justify-between items-center gap-1 sm:gap-8 ${
+                      index % 2 === 0 && 'bg-orange-50/80'
+                    }`}>
+                    <td className='sm:max-w-56 w-full sm:min-w-[20ch] truncate'>
+                      {order.id}
+                    </td>
+                    <td className='w-full flex gap-1'>
+                      <span>{order.product.price}</span>
+                      <span>Kč</span>
+                    </td>
+                    <td className='w-full flex gap-1'>
+                      <span>{order.quantity}</span>
+                      <span>x</span>
+                    </td>
+                    <td className='w-full flex gap-1'>
+                      <span>{order.quantity * order.product.price}</span>
+                      <span>Kč</span>
+                    </td>
+                    <td>
+                      <DeleteButton
+                        id={order.id}
+                        action={deleteOrder}
+                      />
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td className='w-full text-center py-6'>No orders yet</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         ) : (
           <Error message={message} />
         )}
