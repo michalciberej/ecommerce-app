c6214e308b937c27d3390d584f5376f71a8fc936
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-02 18:57:31 +0200

    Add ScrollUpButton component

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import Logo from '@/app/components/Logo';
 import RouteNavigation from '@/app/components/RouteNavigation';
+import ScrollUpButton from '../ScrollUpButton';
 
 const Footer = () => {
   return (
-    <footer className='border-t border-neutral-500 py-4 px-2 flex flex-col gap-y-4 justify-center items-center'>
-      <RouteNavigation />
-      <Logo />
+    <footer className='border-t border-neutral-500 py-4 px-2 relative'>
+      <div className='flex flex-col gap-y-4 justify-center items-center'>
+        <RouteNavigation />
+        <Logo />
+      </div>
+      <ScrollUpButton />
     </footer>
   );
 };
